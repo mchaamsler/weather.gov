04deba036029cf5c3a09eae93bd648820199490d
eric-gade <eric.gade@gsa.gov>
2024-03-21 15:52:04 -0400

    Normalizing our component element prefixes and library names
    -- What
    Specifically, we want to have a universal prefix for all of our
    theme's custom elements. For now we are going with `wx-` since that is
    an abbreviation used internally (we can change later
    easily). Additionally, we have adopted the hyphen in drupal library
    names rather than the underscore, since that's what all the other
    libraries are already using.

--- tests/e2e/cypress/e2e/hourly-forecast-table.cy.js
+++ tests/e2e/cypress/e2e/hourly-forecast-table.cy.js
@@ -7,14 +7,14 @@ describe("Hourly forecast table tests", () => {
     });
 
     it("Should have 2 alert rows on the hourly forecast", () => {
-      cy.get(`#today hourly-table tr[data-row-name="alert"]`).should("have.length", 2);
+      cy.get(`#today wx-hourly-table tr[data-row-name="alert"]`).should("have.length", 2);
     });
 
     it("There is a Red Flag alert of the correct displayed duration", () => {
       // We expect there to be a red-flag alert that spans two hours
       // and that contains the correct event label
       cy.contains(
-        `#today hourly-table tr[data-row-name="alert"]:nth-child(2) td[colspan]:nth-child(2)`,
+        `#today wx-hourly-table tr[data-row-name="alert"]:nth-child(2) td[colspan]:nth-child(2)`,
         "Red Flag Warning"
       )
         .invoke("attr", "colspan")
@@ -24,7 +24,7 @@ describe("Hourly forecast table tests", () => {
     it("Has a Special Weather Statement that begins in the third hour and spans 5 hours", () => {
       cy
         .contains(
-          `#today hourly-table tr[data-row-name="alert"]:nth-child(3) td[colspan]`,
+          `#today wx-hourly-table tr[data-row-name="alert"]:nth-child(3) td[colspan]`,
           "Special Weather Statement")
         .invoke("attr", "colspan")
         .should("equal", "5");
--- web/themes/new_weather_theme/assets/js/components/TabbedNavigator.js
+++ web/themes/new_weather_theme/assets/js/components/TabbedNavigator.js
@@ -194,4 +194,4 @@ class TabbedNavigator extends HTMLElement {
   }
 }
 
-window.customElements.define("tabbed-nav", TabbedNavigator);
+window.customElements.define("wx-tabbed-nav", TabbedNavigator);
